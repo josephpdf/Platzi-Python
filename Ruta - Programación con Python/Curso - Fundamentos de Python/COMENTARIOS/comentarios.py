dc4ecf308a036823f498b940f6bb5982cd6d0b28
josephpdf <josephpf12@hotmail.com>
2026-03-04 16:13:46 -0600

    Create COMENTARIOS

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/COMENTARIOS/comentarios.py
@@ -0,0 +1,16 @@
+# Esta es una línea de comentario
+print("Hola, mundo!")  # Imprime hola, mundo!
+
+# Este
+# es
+# un
+# comentario
+
+'''
+Este
+será
+un comentario
+multilínea
+'''
+
+# print("Esta línea deseo ignorarla")
