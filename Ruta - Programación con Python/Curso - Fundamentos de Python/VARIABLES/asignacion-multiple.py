411fe23cb322c36bdf6eec57f2a49aa24ab07666
josephpdf <josephpf12@hotmail.com>
2026-03-05 08:02:38 -0600

    Create asignacion-multiple.py

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/VARIABLES/asignacion-multiple.py
@@ -0,0 +1,19 @@
+x,y,z = "Manzana", "Naranja", "Banana"
+
+print(x,y,z)
+
+a = b = c = "Mandarina"
+
+print(a,b,c)
+
+print("Mi fruta favorita es " + x)
+print(a + " " + z)
+
+d = 5 
+e = 6
+print(d + e)
+
+nombre = "Joseph"
+apellido = "Poveda"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
