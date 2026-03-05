7e3dd76677e1746b6ddfa6b7906c2400e160d9fd
josephpdf <josephpf12@hotmail.com>
2026-03-05 07:56:07 -0600

    Create VARIABLES

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/VARIABLES/variables.py
@@ -0,0 +1,25 @@
+x = "Esta es una variable"
+print(x)
+
+X = "Esta es OTRA variable"
+print(X)
+
+# Variables correctas
+mivariable = "Esta es una variable" 
+mi_variable = "Esta es una variable con guión bajo"
+_mi_variable = "Esta variable comienza con un guión bajo" # Utilizada para indicar que la variable es privada
+miVariable = "Otra varible"
+MIVARIABLE = "Todo mayus" # Utilizada para indicar que la variable es una constante
+mivariable2 = "Otra variable más"
+
+# Variables incorrectas
+'''
+2variable = "No se puede utilizar esta forma"
+mi-variable = "No se puede utilizar guión medio"
+mi variable = "No se puede utilizar espacios"
+'''
+
+# convenciones de variables
+camelCase = "Comienza con minúscula y el resto de las palabras con mayúscula"
+PascalCase = "Comienza con mayúscula y el resto de las palabras también siguen en mayúscula"
+snake_case = "Se separan las palabras con guiones bajos"
