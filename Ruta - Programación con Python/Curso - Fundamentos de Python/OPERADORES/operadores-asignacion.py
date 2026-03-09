3fee70b98a33463275d7f1fdf7b0302d0df52e0c
josephpdf <josephpf12@hotmail.com>
2026-03-09 17:19:38 -0600

    Create operadores-asignacion.py

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/OPERADORES/operadores-asignacion.py
@@ -0,0 +1,25 @@
+# Operadores Asignación
+
+x = 5
+print(x)
+x += 3 # Suma
+print("Suma:", x)
+x -= 3 # Resta
+print("Resta:", x)
+x *= 3 # Multiplicación
+print("Multiplicación:", x)
+x /= 3 # División
+print("División:", x)
+x %= 2 # Módulo(resto)
+print("Módulo:", x)
+
+y = 20
+print
+y //= 2 # División Entera
+print("División Entera:", y)
+y **= 3 # Exponente 10 * 10 * 10
+print("Exponente:", y)
+
+# WALRUS (morsa) :=
+print(z := 3)
+print(z)
