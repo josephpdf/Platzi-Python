185a2090a693918168ed6231241c61659f600172
josephpdf <josephpf12@hotmail.com>
2026-03-09 16:49:16 -0600

    Create boolenas.py

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/TIPOS-DE-DATOS/booleans.py
@@ -0,0 +1,36 @@
+v = True
+f = False 
+
+print(v)
+print(f)
+print("")
+
+# Comparar valores booleanos
+print(5 > 3) # Verdadero
+print(3 > 5) # Falso
+
+# Tipo de dato
+print(type(v))
+
+# Casteo
+print(bool("Hola Mundo"))
+print(bool(""))
+
+# True
+print(bool("abc"))
+print(bool(123))
+print(bool(["Manzana", "Pera"]))
+
+# False
+print(bool(""))
+print(bool(0))
+print(bool([]))
+print(bool(None))
+
+# Ejemplo de uso de booleanos en una condición
+x = 123.5
+print(isinstance(x, int))
+
+# Ejemplo de uso de booleanos en una condición
+x = 123.5
+print(isinstance(x, int))
