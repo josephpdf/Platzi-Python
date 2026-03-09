758e7fd8f580859de52d60ccb2886e4723e1c63d
josephpdf <josephpf12@hotmail.com>
2026-03-09 16:52:42 -0600

    Cretae none.py

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/TIPOS-DE-DATOS/none.py
@@ -0,0 +1,13 @@
+# Ausencia de valor: None
+x = None 
+print(x)
+print(type(x))
+
+# None es un objeto de tipo NoneType
+# None es un valor singular, es decir, solo existe una instancia de NoneType
+# None es un valor falso en un contexto booleano
+
+print(type(""))
+print(type(0))
+print(type(False))
+print(type([]))
