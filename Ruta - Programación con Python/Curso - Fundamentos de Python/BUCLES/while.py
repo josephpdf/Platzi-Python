0a72667ede7d8e9f4780eb9e186c061007996c0c
josephpdf <josephpf12@hotmail.com>
2026-03-10 10:59:25 -0600

    Create while.py

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/BUCLES/while.py
@@ -0,0 +1,19 @@
+i = 0
+
+# while i <= 10:
+    # print(i)
+    # i += 1
+
+# while i < 10:
+    # print(i)
+    # if i == 5:
+        # break
+    # i += 1
+
+while i < 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i)
+else:
+    print("i dejó de ser menor a 10")
