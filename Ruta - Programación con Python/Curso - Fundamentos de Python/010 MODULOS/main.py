f6835da66f6d182d4e4695a19ecc71d572fcb15b
josephpdf <josephpf12@hotmail.com>
2026-03-11 15:38:24 -0600

    Create MODULOS

---
+++ Ruta - Programación con Python/Curso - Fundamentos de Python/010 MODULOS/main.py
@@ -0,0 +1,7 @@
+# import operaciones # Importamos el módulo operaciones
+from operaciones import sumar, restar, multiplicar, dividir # Importamos funciones específicas del módulo
+
+print(sumar(5, 3))  # Output: 8
+print(restar(5, 3))  # Output: 2
+print(multiplicar(5, 3))  # Output: 15
+print(dividir(5, 3))  # Output: 1.6666666666666667
