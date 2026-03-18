cc810969fbadca4a8c19cf3e7e5eac25d33e95f3
josephpdf <josephpf12@hotmail.com>
2026-03-18 16:56:58 -0600

    Clase 3 - Comprensiones

---
+++ Ruta - Programación con Python/Curso - Python Intermedio para Entornos virtuales y PEP8/comprehensions.py
@@ -0,0 +1,129 @@
+sample_articles = [
+    {
+        "title": "Python logra nuevo éxito",
+        "source": {"name": "TechNews"},
+        "description": "Gran noticia",
+        "category": "Tecnología",
+    },
+    {
+        "title": "Mercado en crisis",
+        "source": {"name": "Finance"},
+        "description": "Análisis completo",
+        "category": "Economía",
+    },
+    {
+        "title": "Nueva tecnología",
+        "source": {"name": "TechNews"},
+        "description": "Innovación",
+        "category": "Tecnología",
+    },
+    {
+        "title": "Deportes hoy",
+        "source": {"name": "Sports"},
+        "description": "Resultados",
+        "category": "Deportes",
+    },
+    {
+        "title": "Política actual",
+        "source": {"name": "News"},
+        "description": "Actualidad",
+        "category": "Política",
+    },
+    {
+        "title": "Ciencia avanza",
+        "source": {"name": "Science"},
+        "description": "Descubrimientos",
+        "category": "Ciencia",
+    },
+]
+
+
+# Retorno de lista tradicional vs comprehension
+def extract_titles_traditional(articles):
+    """Extrae solo los títulos usando un for"""
+    titles = []
+    for article in articles:
+        titles.append(article["title"])
+    return titles
+
+
+def extract_titles(articles):
+    """Extrae solo los títulos usando un comprehension"""
+    return [article["title"] for article in articles]
+
+
+print(extract_titles_traditional(sample_articles))
+print("-------------------------")
+print(extract_titles(sample_articles))
+
+
+print("#################################")
+
+
+# Retorno de lista tradicional vs comprehension con filtro
+def extract_titles_traditional(articles):
+    """Extrae solo los títulos usando un for"""
+    titles = []
+    for article in articles:
+        if len(article["title"]) > 15:  # Filtrar títulos largos
+            titles.append(article["title"])
+    return titles
+
+
+def extract_titles(articles):
+    """Extrae solo los títulos usando un comprehension"""
+    return [
+        article["title"] for article in articles if len(article["title"]) > 15
+    ]  # Filtrar títulos largos
+
+
+print(extract_titles_traditional(sample_articles))
+print("-------------------------")
+print(extract_titles(sample_articles))
+
+
+print("#################################")
+
+
+# Retorno de diccionario tradicional vs comprehension
+def extract_articles_summaries(articles):
+    return {article["title"]: article["description"] for article in articles}
+
+
+def extract_article_summaries_filter(articles):
+    return {
+        article["title"]: article["description"]
+        for article in articles
+        if len(article["description"]) > 15
+    }
+
+
+print(extract_articles_summaries(sample_articles))
+print("-------------------------")
+print(extract_article_summaries_filter(sample_articles))
+
+
+print("#################################")
+
+
+# Reto Lista sin que se repita de todas fuentes usando set, primero  tradicional y luego comprehension
+def extract_name_sources(articles):
+    """Extrae solo los nombres de las fuentes de los artículos sin que se repitan"""
+    sources = set()  # Usamos un set para evitar duplicados
+    for article in articles:  # Iteramos sobre cada artículo
+        sources.add(
+            article["source"]["name"]
+        )  # Agregamos el nombre de la fuente al set
+    return list(sources)  # Convertimos el set a lista para el retorno
+
+
+def extract_name_sources_comprehension(articles):
+    """Extrae solo los nombres de las fuentes de los artículos sin que se repitan usando comprensión de listas."""
+    return list(
+        {article["source"]["name"] for article in articles}
+    )  # Usamos un set comprehension para obtener nombres únicos y luego convertimos a lista
+
+
+print(extract_name_sources(sample_articles))
+print("-------------------------")
+print(extract_name_sources_comprehension(sample_articles))
