0cd872c2cdf33fb41f99c622513e26a000b6ab5b
josephpdf <josephpf12@hotmail.com>
2026-03-19 16:40:51 -0600

    Clase 6 - Formateadores avanzados

--- Ruta - Programación con Python/Curso - Python Intermedio para Entornos virtuales y PEP8/f-strings.py
+++ Ruta - Programación con Python/Curso - Python Intermedio para Entornos virtuales y PEP8/f-strings.py
@@ -1,3 +1,5 @@
+import datetime
+
 name = "Joseph"
 year = 1993
 text = f"Hola, {name}!"  # f-string: cadena formateada con variables
@@ -23,3 +25,50 @@ print("-------------------------")
 edad = 20
 text_if = f"Hola {name}, eres {'mayor de edad' if edad >= 18 else 'menor de edad'}"  # Condicional dentro de f-string
 print(text_if)
+
+print("-------------------------")
+
+bank_balance = 1200000000
+text = f"Tu saldo en la cuenta bancaria es: {bank_balance:,}"
+print(text)
+
+print("-------------------------")
+
+stock_price = 1.405
+text = f"El valor del stock es: {stock_price:.1f}"
+print(text)
+text = f"El valor del stock es: {stock_price:.2f}"
+print(text)
+
+print("-------------------------")
+
+user_id = 3
+text = f"Su id es: {user_id:02d}"
+print(text)
+
+print("-------------------------")
+
+product = "Laptop"
+price = 1000
+
+text = f"Producto: {product} | Precio: {price}"
+print(text)
+
+print("-------------------------")
+
+text = f"Producto: {product:<15} | Precio: {price:>10}"  # Alineación a la izquierda
+print(f"{text}\n{text}\n{text}")  # Repetimos el texto para mostrar la alineación
+
+print("-------------------------")
+
+from datetime import datetime
+
+date = datetime(2024, 12, 5, 10, 10)
+text = f"La fecha completa es: {date}"
+print(text)
+
+print("-------------------------")
+
+date = datetime(2024, 12, 5, 10, 10)
+text = f"La fecha completa es: {date: %A %d de %B de %Y a las %I:%M %p}"  # Formato personalizado para fecha
++print(text)
