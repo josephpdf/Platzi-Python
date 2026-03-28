830db7d9d02a87faa03f73b58fdc780bca9c0746
josephpdf <josephpf12@hotmail.com>
2026-03-28 16:56:28 -0600

    Clase 13 -  Tipado (type hints)

---
+++ Ruta - Programación con Python/Curso - Python Intermedio para Entornos virtuales y PEP8/type-hints.py
@@ -0,0 +1,20 @@
+"""Typing con Python"""
+
+variable = 42  # int
+print(f"Variable: {variable}, del tipo: {type(variable)}")
+
+variable = "Texto de prueba"
+print(f"Variable: {variable}, del tipo: {type(variable)}")
+
+# variable = valor
+# variable: tipo = valor
+
+otra_variable: int = 44
+print(f"Variable: {otra_variable}, del tipo: {type(otra_variable)}")
+
+otra_variable = ""
+
+user_id: int | None = None
+
+# variable = valor
+# variable: tipo = valor
